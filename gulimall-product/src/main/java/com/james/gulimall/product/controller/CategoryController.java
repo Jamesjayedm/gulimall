003572104f911da1802bf0e900c152ec16fee6a5
Jamesjayedm <631967081@qq.com>
2021-06-22 15:59:44 +0800

    三级分类-递归获取树形数据

--- gulimall-product/src/main/java/com/james/gulimall/product/controller/CategoryController.java
+++ gulimall-product/src/main/java/com/james/gulimall/product/controller/CategoryController.java
@@ -1,19 +1,14 @@
 package com.james.gulimall.product.controller;
 
-import java.util.Arrays;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import com.james.common.utils.R;
 import com.james.gulimall.product.entity.CategoryEntity;
 import com.james.gulimall.product.service.CategoryService;
-import com.james.common.utils.PageUtils;
-import com.james.common.utils.R;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 
 
@@ -31,14 +26,14 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * 查出所有分类以及子分类，以树形结构组装起来
      */
-    @RequestMapping("/list")
+    @RequestMapping("/list/tree")
     //@RequiresPermissions("product:category:list")
     public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
+        List<CategoryEntity> entities =  categoryService.listWithTree();
 
-        return R.ok().put("page", page);
+        return R.ok().put("page", entities);
     }
 
 
--- gulimall-product/src/main/java/com/james/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/james/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,16 +1,18 @@
 package com.james.gulimall.product.service.impl;
 
-import org.springframework.stereotype.Service;
-import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.james.common.utils.PageUtils;
 import com.james.common.utils.Query;
-
 import com.james.gulimall.product.dao.CategoryDao;
 import com.james.gulimall.product.entity.CategoryEntity;
 import com.james.gulimall.product.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 
 @Service("categoryService")
@@ -24,6 +26,38 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        // 查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+        // 组装成父子树形结构
+        List<CategoryEntity> levelMenus = entities.stream().filter((categoryEntity) -> categoryEntity.getParentCid() == 0)
+                .map((menu) -> {
+                    menu.setChildren(getChildren(menu, entities));
+                    return menu;
+                })
+                .sorted((menu1, menu2) -> (menu1.getSort()) == null ? 0 : menu1.getSort() - (menu2.getSort() == null ? 0 : menu2.getSort()))
+                .collect(Collectors.toList());
+
+
+
+
+        return levelMenus;
+    }
+
+    // 递归查找所有菜单的子菜单
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> {
+            return categoryEntity.getParentCid().equals(root.getCatId());
+        }).map(categoryEntity -> {
+            // 递归找到子菜单
+            categoryEntity.setChildren(getChildren(categoryEntity, all));
+            return categoryEntity;
+        }).sorted((menu1, menu2) -> (menu1.getSort()) == null ? 0 : menu1.getSort() - (menu2.getSort() == null ? 0 : menu2.getSort())).collect(Collectors.toList());
+        return children;
     }
 
 }
